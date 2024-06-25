017c163307d3be799e940020d6bd80a0327b9654
Steff Brouwer <sbrouwer05@gmail.com>
2024-06-25 11:09:49 +0200

    routes toegevoegd

--- Voedselbank/routes/web.php
+++ Voedselbank/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AllergieController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/allergie', [AllergieController::class, 'index'])->name('allergie.index');
+    Route::post('/allergie', [AllergieController::class, 'store'])->name('allergie.store');
+    Route::get('/allergie/create', [AllergieController::class, 'create'])->name('allergie.create');
+    // Route for showing the form to edit an existing Allergie
+    Route::get('/allergie/{id}/edit', [AllergieController::class, 'edit'])->name('allergie.edit');
+    // Route for updating the Allergie
+    Route::put('/allergie/{id}', [AllergieController::class, 'update'])->name('allergie.update');
+    Route::delete('/allergie/{id}', [AllergieController::class, 'destroy'])->name('allergie.destroy');
 });
 
 require __DIR__.'/auth.php';
